b6bcb62ed55b50961484004a839ab0faa4d1b55f
Antonioni Barros Campos <antonioni.campos@gmail.com>
2021-06-03 18:33:59 -0300

    change to read hdf5 files and other params

--- dataset.py
+++ dataset.py
@@ -1,22 +1,31 @@
-import pyreadr
+import pandas as pd
+import pandas as pd
 import torch
 
 from torch.utils.data import Dataset
 
 class Dataset(Dataset):
 
-    def __init__(self, rdata_file, rdata_key):
-        """
-        Args:
-            csv_file (string): Path to the csv file with data.
-        """
-        self.data = pyreadr.read_r(f'.\\data\\{rdata_file}')[rdata_key].iloc[:, 3:]
-        self.norm_data = (self.data - self.data.mean()) / self.data.std()
+    mean: pd.Series = pd.read_hdf('.\\data\\norm.h5', key='mean')
+    std: pd.Series = pd.read_hdf('.\\data\\norm.h5', key='std')
 
-    def __len__(self):
+    def __init__(self, path: str, key: str, simulation=None):
+        df = pd.read_hdf(path, key=key)
+        if key == 'normal':
+            if simulation:
+                assert isinstance(simulation, int), 'Parâmetro simulation deve ser int com número da simulação'
+                df = df[df['simulationRun'] == simulation]
+            self.data = df.iloc[:, 3:]
+            self.norm_data = (self.data - self.mean) / self.std
+        elif key == 'faulty':
+            assert isinstance(simulation, tuple), 'Parâmetro simulation deve ser tupla com número da simulação e número da falha'
+            self.data = df[(df['simulationRun'] == simulation[0]) & (df['faultNumber'] == simulation[1])].iloc[:, 4:]
+            self.norm_data = (self.data - self.mean) / self.std            
+
+    def __len__(self) -> int:
         return len(self.norm_data)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> dict:
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
